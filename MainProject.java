b85d2b275a52ca3a9a1fc6e55986f27428485e88
jung <ky2059@naver.com>
2025-10-06 11:33:18 +0900

    sample_init

---
+++ MainProject.java
@@ -0,0 +1,10 @@
+// program run
+// compile: javac MainProject.java
+// run: java MainProject
+
+public class MainProject {
+    public static void main(String[] args) { // main function
+
+        System.out.println("Hello world");
+    }
+}
